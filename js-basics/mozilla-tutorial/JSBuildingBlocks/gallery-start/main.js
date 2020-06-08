834df8d2542cd5626341865daf1997e253abdbb1
Manda Tran <mtt040@ucsd.edu>
2020-06-08 12:30:38 -0700

    js image gallery

--- js-basics/mozilla-tutorial/JSBuildingBlocks/gallery-start/main.js
+++ js-basics/mozilla-tutorial/JSBuildingBlocks/gallery-start/main.js
@@ -6,8 +6,27 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    let imageSrc = 'images/pic'+i+'.jpg';
+    newImage.setAttribute('src', imageSrc);
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function() {
+        displayedImage.setAttribute('src', imageSrc);
+    };
+}
+
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(e) {
+    if (btn.getAttribute('class') === 'dark') {
+        btn.setAttribute('class', 'light');
+        e.target.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+    else {
+        btn.setAttribute('class', 'dark');
+        e.target.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+};
